c4bbf50943bef530b0caddf411a317651ca44ced
samirshrestha-tech <128810359+samirshrestha-tech@users.noreply.github.com>
2024-03-07 23:58:34 +1100

    update on routes for products and categories

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./App.css";
 
 import HomePage from "./pages/HomePage";
 import { Route, Routes } from "react-router-dom";
+import Products from "./pages/Products";
+import Categories from "./pages/Categories";
 
 function App() {
   return (
     <div>
-      {/* private routes */}
-
-      {/* public routes */}
-
       <Routes>
+        {/* private routes */}
+
+        {/* public routes */}
         <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </div>
   );
